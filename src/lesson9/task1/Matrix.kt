1311378c93f74137b0e30c2990da0d08aff692cc
ilyak <ilya.kondratev@phystech.edu>
2022-07-16 00:19:57 +0300

    Lesson9 is completed

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -38,32 +38,48 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> =
+    if ((height <= 0) || (width <= 0)) throw IllegalArgumentException() else MatrixImpl(height, width, e)
+
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
 
-    override val width: Int = TODO()
+    val list = MutableList(height) { MutableList(width) { e } }
+    override fun get(row: Int, column: Int): E = list[row][column]
 
-    override fun get(row: Int, column: Int): E  = TODO()
-
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(cell: Cell): E = list[cell.row][cell.column]
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        list[row][column] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        list[cell.row][cell.column] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) =
+        (other is MatrixImpl<*>) && (other.height == height) && (other.width == width) && (other.list == list)
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val builder = StringBuilder()
+        for (row in list) {
+            builder.append("[ ")
+            for (el in row) {
+                builder.append("$el ")
+            }
+            builder.append("]\r\n")
+        }
+        return builder.toString()
+    }
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        return result
+    }
 }
-
